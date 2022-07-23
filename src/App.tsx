c8297fc27991ace1ebe055e0739c2b0d4230effa
renan-eng <54477490+renan-eng@users.noreply.github.com>
2022-07-23 13:35:23 -0300

    remove os tutorias de App.tsx para o arquivo tutoriais.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,8 @@
 import React from 'react';
 import './App.css';
+import Tutoriais from './tutoriais';
 
-//Como declarar variáveis e tipos
-let nome: string;
-let idade: number;
-let idade2: number | string; // a variável pode ser numérica ou uma string
-let ehEstudante: boolean;
-let hobbies: string[]; //Array de strings
-let cargo: [number, string]; //Tuplas
-
-//let pessoa: Object;
-//Como criar uma objeto
-type Pessoa = { 
-  nome: string;
-  idade?: number; //Nesse caso ? permite que essa variável idade seja opcional (sem '?' o objeto precisa ser declarado com essa propriedade caso contrário irá apresentar erro)
-}
-
-let person: Pessoa = {
-  nome: "Renan",
-  idade: 32,
-}
-let muitasPessoas: Pessoa[];
-
+Tutoriais();
 function App() {
   return (
     <div className="App">
@@ -29,13 +10,5 @@ function App() {
     </div>
   );
 }
-
-
-//Criando funções
-function printNome(nome: string) { //nome : string precisa ser declado o tipo esperado para cada variável
-  console.log(nome);
-}
-
-printNome("Renan");
 
 export default App;
