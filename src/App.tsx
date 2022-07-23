9cad677cfcec647a690909bb4e92ba6e3e845fa2
renan-eng <54477490+renan-eng@users.noreply.github.com>
2022-07-23 12:57:45 -0300

    Como declarar variáveis e objetos em TS

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-let name = "Renan";
+//Como declarar variáveis e tipos
+let nome: string;
+let idade: number;
+let idade2: number | string; // a variável pode ser numérica ou uma string
+let ehEstudante: boolean;
+let hobbies: string[]; //Array de strings
+let cargo: [number, string]; //Tuplas
+
+//let pessoa: Object;
+//Como criar uma objeto
+type Pessoa = { 
+  nome: string;
+  idade?: number; //Nesse caso ? permite que essa variável idade seja opcional (sem '?' o objeto precisa ser declarado com essa propriedade caso contrário irá apresentar erro)
+}
+
+let person: Pessoa = {
+  nome: "Renan",
+  idade: 32,
+}
+let muitasPessoas: Pessoa[];
 
 function App() {
   return (
