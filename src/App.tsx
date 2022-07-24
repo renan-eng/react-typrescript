5e5891f6095d1192130623a14eba361bdcfcb149
renan-eng <54477490+renan-eng@users.noreply.github.com>
2022-07-24 08:39:52 -0300

    Atualizado o componente InputField para enviar a função handleAdd

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <span className='heading'>Tarefas</span>
-      <InputField todo={todo} setTodo={setTodo} />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
     </div>    
   );
 }
