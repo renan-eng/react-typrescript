e203886c1999db738529164a63b00b2782843620
renan-eng <54477490+renan-eng@users.noreply.github.com>
2022-07-24 08:13:46 -0300

    criado atualização de estado para quando o usuario digitar um valor no input, esse valor será capturado pela função setTodo e salvo na variável todo

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -9,7 +9,13 @@ interface Props {
 const InputField = ({ todo, setTodo }: Props) => {
   return (
     <form className='input'>
-        <input type='input' placeholder='Enter a task' className='input__box'/>
+        <input type='input'
+            value={todo}
+            onChange={
+                (e) => setTodo(e.target.value)
+            } 
+            placeholder='Enter a task' className='input__box'
+        />
         <button className='input__submit' type='submit'>+</button>
     </form>
   )
