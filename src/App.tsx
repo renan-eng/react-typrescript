83ac3a70ddffb28a6b2570aadd590fefee2d76e2
renan-eng <54477490+renan-eng@users.noreply.github.com>
2022-07-24 09:17:53 -0300

    Atualizando a função handleAdd
    Acrescentado a funcionalidade na função para criar um vetor com os todos anteriores (...todos), objeto com id gerado com a função Date.now(), valor atual todo (após submit), isDone que sera verificano no futuro se a tarefa foi concluida.

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,23 @@ const App: React.FC = () => {
 
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault(); //Evita que a página seja recarrega ao pressionar submit
+    
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo("");
+    }
   };
 
-  //console.log(todo); //checando se o componente InputField esta recebendo todo
+  console.log(todos); //checando se o componente InputField esta recebendo todo
 
   return (
     <div className="App">
       <span className='heading'>Tarefas</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      {/* <TodoList />: */}
+      {todos.map((t)=>(
+      <li>{t.todo}</li>
+      ))}
     </div>    
   );
 }
