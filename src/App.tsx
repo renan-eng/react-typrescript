27679e9d94b07194614f996cf129dabb9e4356e9
renan-eng <54477490+renan-eng@users.noreply.github.com>
2022-07-24 08:10:30 -0300

    Teste pra testar se a variável todo esta recebendo os inputs do usuário

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import InputField from './components/InputField'
 Tutoriais();
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
+
+  //console.log(todo); //checando se o componente InputField esta recebendo todo
+
   return (
     <div className="App">
       <span className='heading'>Tarefas</span>
