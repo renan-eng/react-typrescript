233430868586f5bf3bc1611f8868637dfbd48a20
renan-eng <54477490+renan-eng@users.noreply.github.com>
2022-07-24 05:54:00 -0300

    Interface vs type
    Inclui alguns comentários e exemplos para explicar um pouco sobre Type Aliases: type e interface. Além disso explico a diferença entre elas.

--- src/tutoriais.tsx
+++ src/tutoriais.tsx
@@ -7,19 +7,35 @@ let ehEstudante: boolean;
 let hobbies: string[]; //Array de strings
 let cargo: [number, string]; //Tuplas
 
-//let pessoa: Object;
-//Como criar uma objeto
+//let pessoa: Object; Para criar um objeto pessoa porém não é muito utilizado
+//Type Aliases = permite criar uma referência que está relacionada a um ou mais tipos
 type Pessoa = { 
   nome: string;
   idade?: number; //Nesse caso ? permite que essa variável idade seja opcional (sem '?' o objeto precisa ser declarado com essa propriedade caso contrário irá apresentar erro)
 }
 
-let person: Pessoa = {
+let pessoa: Pessoa = {
   nome: "Renan",
   //idade: 32, //é opcional pq o objeto pessoa possui o atributo 'idade?: number' -> '?'
 }
 
-let muitasPessoas: Pessoa[]; // para croar um Array de objetos Pessoa
+//Interface - a diferença entre type e interface está na maneira como elas podem ser extendidas
+//interface pode ser sempre extendida para adicionar novas propriedades
+//interface Exemplo extends Extenção {varC: boolean}
+//type é mais restrita nesse caso e pode ser extendida via interseção: type A = b & {var: string}
+interface Exemplo {
+  varA: string;
+  varB: number;
+}
+
+let exemplo: Exemplo = {
+  varA: "lçaskdjfasdf",
+  varB: 13345,
+}
+ console.log(`Pessoa.nome: ${pessoa.nome}`);
+ console.log(`Exemplo.varA: ${exemplo.varA}\nExemplo.varB: ${exemplo.varB} `);
+
+let muitasPessoas: Pessoa[]; // para criar um Array de objetos Pessoa
 
 
 //Criando funções
