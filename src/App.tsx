98a86f54a2e145261ef810945a8fbcf3eda3b31a
renan-eng <54477490+renan-eng@users.noreply.github.com>
2022-07-25 06:15:22 -0300

    Comentário adicionado explicando a função handleAdd()

--- src/App.tsx
+++ src/App.tsx
@@ -10,16 +10,27 @@ const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
   const [todos, setTodos] = useState<Todo[]>([]);
 
+  /*
+  * A função handleAdd() tem dois objetivos:
+  *   1. e.preventDefault() -> impede o carregamento da pagina após o usuário
+  *      clicar em no botão +
+  *   2. if (todo) -> verificar se o usuário digitou algo na caixa de input e
+  *      capturar tudo e agrupar em um array com 3 elementos:
+  *         1. id     -> numero único aleatório gerado apartir da data atual Date.now()
+  *         2. todo   -> o que foi digitado pelo usuário
+  *         3. isDone -> false ou true, sera usado para permitir a conclusão da tarefa
+  *   3. após realizar os passo anteriores 
+  */
   const handleAdd = (e: React.FormEvent) => {
-    e.preventDefault(); //Evita que a página seja recarrega ao pressionar submit
-    
+    e.preventDefault();    
     if (todo) {
       setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
       setTodo("");
     }
   };
 
-  console.log(todos); //checando se o componente InputField esta recebendo todo
+  // console.log(todo); //checando se o componente InputField esta recebendo todo
+  // console.log(todos); //checando se o componente InputField esta recebendo todo
 
   return (
     <div className="App">
