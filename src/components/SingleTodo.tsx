6e16b91b116edf4c31d68e8905ff8548590894f4
renan-eng <54477490+renan-eng@users.noreply.github.com>
2022-07-26 04:15:11 -0300

    Atualizando SingleTodo com todas as propriedades e tipos pertinentes.

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import { Todo } from '../model';
 
 const singleTodo = () => {
+type Props = {
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+const singleTodo = ({ todo, todos, setTodos}:Props) => {
   return (
     <div>singleTodo</div>
   )
