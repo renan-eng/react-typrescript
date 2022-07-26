c987365e02d1288b001f59120c9d4000974e48d6
renan-eng <54477490+renan-eng@users.noreply.github.com>
2022-07-26 04:16:14 -0300

    Exibindo a tarefa em tag span e incluindo os icones responsaveis por editar, excluir e marcar como concluido.

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import { Todo } from '../model';
+import { AiFillEdit, AiFillDelete  } from 'react-icons/ai'
+import { MdDone } from 'react-icons/md'
+import './styles.css'
+
 
 const singleTodo = () => {
 type Props = {
@@ -10,7 +14,20 @@ type Props = {
 
 const singleTodo = ({ todo, todos, setTodos}:Props) => {
   return (
-    <div>singleTodo</div>
+    <form className='todos__single' >
+      <span className='todos__single--text'>{todo.todo}</span>
+      <div className="className">
+        <span className="icon">
+          <AiFillEdit /> 
+        </span>
+        <span className="icon"> 
+          <AiFillDelete /> 
+        </span>
+        <span className="icon">
+          <MdDone /> 
+        </span>
+      </div>
+    </form>
   )
 }
 
